e9e8d8fb3b749012165f9367641cd20e3b467992
mirellaxsj <mirellafrancasenai@gmail.com>
2024-03-13 11:01:47 -0300

    transportadora

---
+++ api/src/routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const routes = express.Router();
+
+const Cliente = require('./controllers/cliente');
+const Veiculo = require('./controllers/veiculo');
+
+routes.get('/', (req, res) => {
+    res.json("API Transportadora XPTO 1.0")
+});
+
+routes.post('/clientes', Cliente.addCliente);
+routes.get('/clientes', Cliente.getClientes);
+routes.get('/clientes/:id', Cliente.getClientes);
+routes.put('/clientes', Cliente.updateCliente);
+routes.delete('/clientes/:id', Cliente.deleteCliente);
+
+routes.post('/veiculos', Veiculo.addVeiculo);
+routes.get('/veiculos', Veiculo.getVeiculos);
+routes.get('/veiculos/:placa', Veiculo.getVeiculos);
+routes.put('/veiculos', Veiculo.updateVeiculo);
+routes.delete('/veiculos/:placa', Veiculo.deleteVeiculo);
+
+routes.post('/funcionarios', Funcionario.addFuncionario);
+routes.get('/funcionarios', Funcionario.getFuncionario);
+routes.get('/funcionarios', Funcionario.getFuncionario);
+routes.put('/funcionarios', Funcionario.updateFuncionario);
+routes.delete('/funcionarios', Funcionario.deleteFuncionario);
+module.exports = routes;
